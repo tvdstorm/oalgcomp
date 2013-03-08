5a7ab52c5e6081c476ee0d3260ea3ce347754eaf
Tijs van der Storm <storm@cwi.nl>
2013-03-08 11:58:11 +0100

    Added EPL license notes.

--- src/oalg/algebra/aspects/Circular.scala
+++ src/oalg/algebra/aspects/Circular.scala
@@ -4,6 +4,11 @@
  * - Tijs van der Storm (storm@cwi.nl)
  * - Alex Loh (alexloh@cs.utexas.edu)
  * - William R. Cook (wcook@cs.utexas.edu)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
 ********************************************************************************/
 
 package oalg.algebra.aspects
